3fc95aaa3f2115d10db8c979b32c8f8c00120d39
baranselgok <59847659+baranselgok@users.noreply.github.com>
2022-09-21 18:47:57 +0300

    Update feeCollector.sol

--- feeCollector.sol
+++ feeCollector.sol
@@ -6,15 +6,15 @@ contract feeCollector{
     uint256 public balance;
 
     constructor(){
-        owner = msg.sender;
+        owner = msg.sender; //msg.sender stands for the wallet that is currently interacting with the contract. 
     }
 
     receive() payable external{
-        balance += msg.value;
+        balance += msg.value; //msg.value is the amoun of ether sent in the transactions.
     }
 
     function withdraw(uint amount, address payable destAddr) public   {
-        require(msg.sender == owner, "Only the owner can withdraw.");
+        require(msg.sender == owner, "Only the owner can withdraw.");  //sets the privacy
         require(amount <= balance, "Insufficient balance");
 
         destAddr.transfer(amount);
